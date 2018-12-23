0abaef11007b34560d4bf1c068a10c3b230fa76f
shashwatanand <shashwat.anand@gmail.com>
2018-12-23 13:12:48 +0100

    populating student list from firestore

--- angular/StudentFireStoreApp/src/app/shared/student.service.ts
+++ angular/StudentFireStoreApp/src/app/shared/student.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Student } from './student.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
   formData : Student;
-  constructor() { }
+  constructor(private fireStore : AngularFirestore) { }
+
+  getStudents() {
+    return this.fireStore.collection("students").snapshotChanges();
+  }
 }
--- angular/StudentFireStoreApp/src/app/students/student-list/student-list.component.ts
+++ angular/StudentFireStoreApp/src/app/students/student-list/student-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/shared/student.service';
+import { Student } from 'src/app/shared/student.model';
 
 @Component({
   selector: 'app-student-list',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-list.component.css']
 })
 export class StudentListComponent implements OnInit {
-
-  constructor() { }
+  list : Student[];
+  constructor(private service : StudentService) { }
 
   ngOnInit() {
+    this.service.getStudents().subscribe(actionArray => {
+      this.list = actionArray.map(item => {
+        return {
+          id : item.payload.doc.id,
+          ...item.payload.doc.data()
+        } as Student
+      })
+    });
   }
 
 }
