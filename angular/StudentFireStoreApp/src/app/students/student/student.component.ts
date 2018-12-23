79598d48a3331fdf565b5d3a9d26c0573a68e8a8
shashwatanand <shashwat.anand@gmail.com>
2018-12-23 12:04:33 +0100

    added validation

--- angular/StudentFireStoreApp/src/app/students/student/student.component.ts
+++ angular/StudentFireStoreApp/src/app/students/student/student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentService } from 'src/app/shared/student.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-student',
@@ -11,6 +12,20 @@ export class StudentComponent implements OnInit {
   constructor(private service : StudentService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form? : NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      id : null,
+      studentId : "",
+      fullName : "",
+      dept : "",
+      mobile : ""
+    }
   }
 
 }
