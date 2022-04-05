de7bca72f2dfc19826ac091857bb236dd4027bc6
Shashwat Anand <shashwat.anand@gmail.com>
2022-04-05 14:48:55 +0200

    initail commit

---
+++ spring/spring-pact/src/main/java/in/shashwatanand/springpact/SpringPactApplication.java
@@ -0,0 +1,13 @@
+package in.shashwatanand.springpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPactApplication.class, args);
+	}
+
+}
---
+++ spring/spring-pact/src/test/java/in/shashwatanand/springpact/SpringPactApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shashwatanand.springpact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
