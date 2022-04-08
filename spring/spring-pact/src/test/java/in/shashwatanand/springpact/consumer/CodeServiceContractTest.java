e7dd3379125be88bd3f698aaced3a57ce5175a33
Shashwat Anand <shashwat.anand@gmail.com>
2022-04-08 20:50:38 +0200

    updating code

--- spring/spring-pact/src/test/java/in/shashwatanand/springpact/consumer/CodeServiceContractTest.java
+++ spring/spring-pact/src/test/java/in/shashwatanand/springpact/consumer/CodeServiceContractTest.java
@@ -9,10 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
 
 import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
@@ -20,7 +18,6 @@ import au.com.dius.pact.core.model.PactSpecVersion;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import in.shashwatanand.springpact.CodeControllerClient;
-import in.shashwatanand.springpact.model.JavaCode;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = "user-service.base-url:http://localhost:8080", classes = CodeControllerClient.class)
 @ExtendWith(PactConsumerTestExt.class)
@@ -29,7 +26,7 @@ public class CodeServiceContractTest {
 	@Autowired
 	private CodeControllerClient codeControllerClient;
 
-	@Pact(consumer = "code-consumer")
+	@Pact(consumer = "code-consumer", provider = "code-provider")
 	public RequestResponsePact pactForHelloWorld(PactDslWithProvider builder) {
 		Map<String, String> reqHeaders = new HashMap<>();
 		reqHeaders.put("content-type", "text/plain");
@@ -40,13 +37,14 @@ public class CodeServiceContractTest {
 					.path("/hello")
 					.method("GET")
 				.willRespondWith().status(200)
+					.headers(reqHeaders)
 					//.body(new PactDslJsonBody().stringType("Hello from spring with pact project"))
 					.body("Hello from spring with pact project")
 					//.body(LambdaDsl.newJsonBody((o) -> o.stringType("Hello from spring with pact project")).build())
 				.toPact();
 	}
 	
-	@Pact(consumer = "code-consumer")
+	@Pact(consumer = "code-consumer", provider = "code-provider")
 	public RequestResponsePact pactForGenJavaCode(PactDslWithProvider builder) {
 		Map<String, String> reqHeaders = new HashMap<>();
 		reqHeaders.put("content-type", "text/plain");
@@ -71,7 +69,8 @@ public class CodeServiceContractTest {
 	@PactTestFor(pactMethod = "pactForGenJavaCode", pactVersion = PactSpecVersion.V3)
 	@Test
 	public void genJavaCode(MockServer mockServer) {
-		JavaCode javaCode = codeControllerClient.genJavaCode(mockServer.getUrl());
+		codeControllerClient.genJavaCode(mockServer.getUrl());
+		//JavaCode javaCode = codeControllerClient.genJavaCode(mockServer.getUrl());
 		//assertThat(returnStr).isEqualTo("Hello from spring with pact project");
 	}
 }
--- spring/spring-pact/src/test/java/in/shashwatanand/springpact/provider/CodeServiceProviderTest.java
+++ spring/spring-pact/src/test/java/in/shashwatanand/springpact/provider/CodeServiceProviderTest.java
@@ -1,5 +1,47 @@
 package in.shashwatanand.springpact.provider;
 
-public class CodeServiceProviderTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Provider("code-provider")
+@PactBroker(host = "localhost", port = "9292")
+@IgnoreNoPactsToVerify
+@Disabled
+public class CodeServiceProviderTest {
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void before(PactVerificationContext context) {
+		System.out.println("Port : " + port);
+		context.setTarget(new HttpTestTarget("localhost", port));
+	}
+
+	@TestTemplate
+	@ExtendWith(PactVerificationInvocationContextProvider.class)
+	void pactVerificationTestTemplate(PactVerificationContext context) {
+		context.verifyInteraction();
+	}
+	
+	@State("hello world")
+	public void testHelloWorld() {}
+	
+	@State("Gen Java code")
+	public void testGenJavaCode() {}
 }
