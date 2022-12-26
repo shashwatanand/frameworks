6f2044dd9b6c6c3ee908366d7592dc0819cfb238
Shashwat Anand <shashwat.anand@gmail.com>
2022-12-26 13:48:11 +0100

    .
    Reason:
    Updated frontend
    
    Fixes:

---
+++ mern/soccer/frontend/src/Components/Player/PlayerForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class PlayerForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (
+            <div>
+                PlayerForm
+            </div>
+        );
+    }
+}
+ 
+export default PlayerForm;
---
+++ mern/soccer/frontend/src/Components/Player/PlayerSingle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayerSingle = () => {
+    return (
+        <div>
+            PlayerSingle
+        </div>
+    );
+}
+ 
+export default PlayerSingle;
